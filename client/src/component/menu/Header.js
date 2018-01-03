f321efa091e014c4fe08b80f9d1bbc8037e96991
Brent <brentgammon94@gmail.com>
2018-01-03 20:42:53 +0000

    basic styling for home page non logged in

--- client/src/component/menu/Header.js
+++ client/src/component/menu/Header.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-
+import "./header.scss";
 class Header extends Component {
   render() {
     return (
-      <div>
+      <div className="header">
         <h1>Apple Watch Fitness Data</h1>
       </div>
     );
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Routing from "./Routing";
 import registerServiceWorker from "./registerServiceWorker";
+import "./index.scss";
 
 ReactDOM.render(<Routing />, document.getElementById("root"));
 registerServiceWorker();
